da63e624ca922629a5b64cc278292e49f72f1f22
Cian Butler <67752829+ciandevelops@users.noreply.github.com>
2023-05-18 18:08:19 -0400

    src folder created

---
+++ src/app.py
@@ -0,0 +1,11 @@
+# app.py
+# This is a test commit
+def add(a, b):
+    return a + b
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(1, -1) == 0
+    
+#this is updated
+#this is after the action is in there 
